e79398c56ef87a3590c1fdd96b069cab0d453dc6
sys35779365-del <sys3577.9365@gmail.com>
2026-08-25 17:42:10 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pygame,sys,random
 pygame.init()
+info = pygame.display.Info()
+width = info.current_w
+height = info.current_h
 screen = pygame.display.set_mode()
+
+i_s_b = pygame.image.load('src\\img\\slime_back.png')
+i_s_b = pygame.transform.scale(i_s_b,(50,50))
+i_s_f = pygame.image.load('src\\img\\slime_front.png')
+i_s_f = pygame.transform.scale(i_s_f,(50,50))
+i_s_l = pygame.image.load('src\\img\\slime_lr.png')
+i_s_l = pygame.transform.scale(i_s_l,(50,50))
+i_s_r = pygame.transform.flip(i_s_l,1,0)
+
+p_rect = pygame.Rect(0,0,50,50)
+p_i = i_s_f
 running = True
 LCTRL = False
 while running:
@@ -15,4 +29,20 @@ while running:
         if event.type == pygame.KEYUP:
             if event.type == pygame.K_LSHIFT:
                 LCTRL = False
+    keyInput = pygame.key.get_pressed()
+    if keyInput[pygame.K_LEFT] and p_rect.left > 0:
+        p_rect.left -= 1
+        p_i = i_s_l
+    if keyInput[pygame.K_RIGHT] and p_rect.right < width:
+        p_rect.left += 1
+        p_i = i_s_r
+    if keyInput[pygame.K_UP] and p_rect.top > 0:
+        p_rect.top -= 1
+        p_i = i_s_f
+    if keyInput[pygame.K_DOWN] and p_rect.bottom < height:
+        p_rect.top += 1
+        p_i = i_s_b
+    screen.fill((0,0,0))
+    screen.blit(p_i,p_rect)
+    pygame.display.update()
 #Kevin test 입니다.
