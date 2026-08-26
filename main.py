457595448225538e1e719eeef30c8f94bfebb4f2
Megiddo-Cmd <albert0424@kakao.com>
2026-08-26 17:06:07 +0900

    modified:   main.py 	renamed:    src/img/slime_back.png -> src/img/player/slime_back.png 	renamed:    src/img/slime_back_lr.png -> src/img/player/slime_back_lr.png 	renamed:    src/img/slime_front.png -> src/img/player/slime_front.png 	renamed:    src/img/slime_lr.png -> src/img/player/slime_lr.png 	renamed:    src/img/stage1/stage1_1.png -> src/img/stage1/stage1_1.png 	renamed:    src/img/stage1/stage1_2.png -> src/img/stage1/stage1_2.png

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ pygame.init()
 info = pygame.display.Info()
 width = info.current_w
 height = info.current_h
-screen = pygame.display.set_mode()
+screen = pygame.display.set_mode((width, height))
 
 #경로지정
-path_back = os.path.join("src", "img", "slime_back.png")
-path_front = os.path.join("src", "img", "slime_front.png")
-path_lr = os.path.join("src", "img", "slime_lr.png")
-path_back_lr = os.path.join("src", "img", "slime_back_lr.png")
+path_back = os.path.join("src", "img", "player", "slime_back.png")
+path_front = os.path.join("src", "img", "player", "slime_front.png")
+path_lr = os.path.join("src", "img", "player", "slime_lr.png")
+path_back_lr = os.path.join("src", "img", "player", "slime_back_lr.png")
+path_stage1_1 = os.path.join("src", "img", "stage1", "stage1_1.png")
+path_stage1_2 = os.path.join("src", "img", "stage1", "stage1_2.png")
+
 
 #후방
 i_s_b = pygame.image.load(path_back)
@@ -25,13 +28,22 @@ i_s_r = pygame.transform.flip(i_s_l,1,0)
 i_s_bl = pygame.image.load(path_back_lr)
 i_s_bl = pygame.transform.scale(i_s_bl, (50, 50))
 i_s_br = pygame.transform.flip(i_s_bl, 1, 0)
+TILE_SIZE = (128, 128)
+
+stage1_images = [
+    pygame.transform.scale(pygame.image.load(path_stage1_1), TILE_SIZE),
+    pygame.transform.scale(pygame.image.load(path_stage1_2), TILE_SIZE)
+]
 
 #후방좌우 이미지를 아래쪽 화살표와 좌/우 화살표를 동시에 눌렀을 때 사용하도록 설정 필요 - Megiddo-Cmd가 완료
 
-p_rect = pygame.Rect(0,0,50,50)
+p_rect = pygame.Rect(width // 2, height // 2, 50, 50)
 p_i = i_s_f
 running = True
 LCTRL = False
+
+clock = pygame.time.Clock()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,24 +56,46 @@ while running:
         if event.type == pygame.KEYUP:
             if event.type == pygame.K_LSHIFT:
                 LCTRL = False
+
     keyInput = pygame.key.get_pressed()
-    if keyInput[pygame.K_LEFT] and p_rect.left > 0:
+    if keyInput[pygame.K_LEFT]:
         p_rect.left -= 2
         p_i = i_s_l
-    if keyInput[pygame.K_RIGHT] and p_rect.right < width:
+    if keyInput[pygame.K_RIGHT]:
         p_rect.left += 2
         p_i = i_s_r
-    if keyInput[pygame.K_UP] and p_rect.top > 0:
+    if keyInput[pygame.K_UP]:
         p_rect.top -= 2
         p_i = i_s_f
-    if keyInput[pygame.K_DOWN] and p_rect.bottom < height:
+    if keyInput[pygame.K_DOWN]:
         p_rect.top += 2
         p_i = i_s_b
-        if keyInput[pygame.K_LEFT] and p_rect.left > 0:
+        if keyInput[pygame.K_LEFT]:
             p_i = i_s_bl
-        if keyInput[pygame.K_RIGHT] and p_rect.right < width:
+        elif keyInput[pygame.K_RIGHT]:
             p_i = i_s_br
+
     screen.fill((255,255,255))
-    screen.blit(p_i,p_rect)
+
+    cam_x = p_rect.centerx - width // 2
+    cam_y = p_rect.centery - height // 2
+    chunk_size = TILE_SIZE[0] * 2  # 128 * 2 = 256 (쿼드블록 전체 가로/세로 크기)
+    
+    start_x = -(p_rect.x % chunk_size)
+    start_y = -(p_rect.y % chunk_size)
+
+    for bx in range(start_x - chunk_size, width + chunk_size, chunk_size):
+        for by in range(start_y - chunk_size, height + chunk_size, chunk_size):
+            # 2개의 타일(stage1_images[0], stage1_images[1])을 2x2 쿼드블록으로 배치
+            # 예시 구조 (랜덤 섞임에 따라 2x2 형태가 쿼드블록 단위로 반복됨):
+            screen.blit(stage1_images[0], (bx, by))                               # 좌상단 (1행 1열)
+            screen.blit(stage1_images[1], (bx + TILE_SIZE[0], by))                # 우상단 (1행 2열)
+            screen.blit(stage1_images[1], (bx, by + TILE_SIZE[1]))                # 좌하단 (2행 1열)
+            screen.blit(stage1_images[0], (bx + TILE_SIZE[0], by + TILE_SIZE[1])) # 우하단 (2행 2열)
+
+    player_screen_pos = (width // 2 - 25, height // 2 - 25)
+    screen.blit(p_i, player_screen_pos)
+
     pygame.display.update()
+    clock.tick(60)
 #Kevin test 입니다.
