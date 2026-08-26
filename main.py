99fcc8fffca500e65ed9cd21df847eabe7821647
Megiddo-Cmd <albert0424@kakao.com>
2026-08-26 15:46:48 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ i_s_bl = pygame.transform.scale(i_s_bl, (50, 50))
 i_s_br = pygame.transform.flip(i_s_bl, 1, 0)
 
 #후방좌우 이미지를 아래쪽 화살표와 좌/우 화살표를 동시에 눌렀을 때 사용하도록 설정 필요
+
 p_rect = pygame.Rect(0,0,50,50)
 p_i = i_s_f
 running = True
