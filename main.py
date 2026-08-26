47086b7c3fb4d4ad918c3748dc607f86ce973a1c
Megiddo-Cmd <albert0424@kakao.com>
2026-08-26 17:13:27 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -13,20 +13,22 @@ path_back_lr = os.path.join("src", "img", "player", "slime_back_lr.png")
 path_stage1_1 = os.path.join("src", "img", "stage1", "stage1_1.png")
 path_stage1_2 = os.path.join("src", "img", "stage1", "stage1_2.png")
 
+player_size = 70
+player_speed = 3
 
 #후방
 i_s_b = pygame.image.load(path_back)
-i_s_b = pygame.transform.scale(i_s_b,(50,50))
+i_s_b = pygame.transform.scale(i_s_b,(player_size,player_size))
 #전방
 i_s_f = pygame.image.load(path_front)
-i_s_f = pygame.transform.scale(i_s_f,(50,50))
+i_s_f = pygame.transform.scale(i_s_f,(player_size,player_size))
 #좌우
 i_s_l = pygame.image.load(path_lr)
-i_s_l = pygame.transform.scale(i_s_l,(50,50))
+i_s_l = pygame.transform.scale(i_s_l,(player_size,player_size))
 i_s_r = pygame.transform.flip(i_s_l,1,0)
 #후방 좌우
 i_s_bl = pygame.image.load(path_back_lr)
-i_s_bl = pygame.transform.scale(i_s_bl, (50, 50))
+i_s_bl = pygame.transform.scale(i_s_bl, (player_size, player_size))
 i_s_br = pygame.transform.flip(i_s_bl, 1, 0)
 TILE_SIZE = (128, 128)
 
@@ -37,7 +39,7 @@ stage1_images = [
 
 #후방좌우 이미지를 아래쪽 화살표와 좌/우 화살표를 동시에 눌렀을 때 사용하도록 설정 필요 - Megiddo-Cmd가 완료
 
-p_rect = pygame.Rect(width // 2, height // 2, 50, 50)
+p_rect = pygame.Rect(width // 2, height // 2, player_size, player_size)
 p_i = i_s_f
 running = True
 LCTRL = False
@@ -59,16 +61,16 @@ while running:
 
     keyInput = pygame.key.get_pressed()
     if keyInput[pygame.K_LEFT]:
-        p_rect.left -= 2
+        p_rect.left -= player_speed
         p_i = i_s_l
     if keyInput[pygame.K_RIGHT]:
-        p_rect.left += 2
+        p_rect.left += player_speed
         p_i = i_s_r
     if keyInput[pygame.K_UP]:
-        p_rect.top -= 2
+        p_rect.top -= player_speed
         p_i = i_s_f
     if keyInput[pygame.K_DOWN]:
-        p_rect.top += 2
+        p_rect.top += player_speed
         p_i = i_s_b
         if keyInput[pygame.K_LEFT]:
             p_i = i_s_bl
